fa7d4ec22fd39918d2d94b02efab39e93d90aae2
andressantage <ingeniero.andres.santana@gmail.com>
2023-05-24 23:06:55 -0500

    feat: :sparkles: cambios: 1,2,3,4,5,6
    Seguire trabajando

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
+let nombre
+let edad
+let sexo
+let persona1
 document.getElementById('miFormulario').addEventListener('submit', function(event) {
     event.preventDefault(); 
-    var nombre = document.getElementById('nombre').value;
-    var edad = document.getElementById('edad').value;
-    var sexo = document.getElementById('sexo').value;
-
-    let persona1=new Persona(nombre,edad,sexo);
+    nombre = document.getElementById('nombre').value;
+    edad = document.getElementById('edad').value;
+    sexo = document.getElementById('sexo').value;
+    persona1=new Persona(nombre,edad,sexo);
     document.getElementById("saludo").textContent="Resultado: "+persona1.saludar()
     console.log(persona1.saludar())
 });
@@ -21,4 +24,36 @@ class Persona{
     saludar(){
         return `Hola ${this.nombre} buen dia` 
     }
+    static esMayorDeEdad(){
+        if(this.edad>=18){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
+
+class Estudiante extends Persona{
+    constructor(nombre,edad,sexo,carrera){
+        super(nombre,edad,sexo)
+        this.carrera=carrera;
+    }
+    estudiar(){
+        return `La persona ${this.nombre} estudia ${this.carrera}` 
+    }
+}
+
+document.getElementById('miFormulario1').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    let carrera = document.getElementById('carrera').value;
+
+    let estudiante1=new Estudiante(nombre,edad,sexo,carrera);
+    document.getElementById("imprimirSaludo").textContent=estudiante1.saludar()
+    document.getElementById("imprimirCarrera").textContent=estudiante1.estudiar()
+});
+
+document.getElementById('miFormulario2').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    document.getElementById("imprimirResEdad").textContent=Persona.esMayorDeEdad(persona1.edad)
+});
+
