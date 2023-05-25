3c98185ac0f5d1bd85dec8e0386637611ca70318
andressantage <ingeniero.andres.santana@gmail.com>
2023-05-24 23:51:15 -0500

    feat: :sparkles: Agrego 7,8,9
    A terminar

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# TallerPOO-Basic
+# English-App
+
+## Obejtivos desarrollo inicial
+Aplicacion de ingles para aprender vocabulario de 1200 palabras
+
+## Descripcion
+Aplicacion de ingles para aprender vocabulario de 3000 palabras
+
+### Caracteristicas de la aplicación web
+- 3000 palabras con su respectiva traduccion a ingles y español
+- Lenguje fonético de las palabras
+- Palabras seccionadas en los diferentes tipos de palabra y clase
+- Audio de las palabras en ingles americano y algunas en britanico
+- Secciones de practica para listening (escucha), y speaking, writing y reading en una sola sección
+- Aplicativo con diseño responsivo para smartphone y pc
+- Diseño personalizable
+
+#### Diseño personalizable
+
+En base a que el usuario ya tiene cierto vocabulario la aplicación tiene un diseño para
+que al dar click el usuario en unos botones pueda definir que palabras le muestre con 
+mayor frecuencia para de esta forma usar el metodo de memorización espaciada y se pueda
+generar retención a largo plazo en la memoria.
+
+La aplicación tiene la posibilidad de cambiar la imagen de la palabra, enviar feedback y
+con modo nocturno y diurno
--- main.js
+++ main.js
@@ -56,4 +56,47 @@ document.getElementById('miFormulario2').addEventListener('submit', function(eve
     event.preventDefault(); 
     document.getElementById("imprimirResEdad").textContent=Persona.esMayorDeEdad(persona1.edad)
 });
+///////////
+class Animal{
+    constructor(nombre,edad){
+        this.nombre=nombre;
+        this.edad=edad;
+    }
+    hacerSonido(){
+        return `Sonido del animal ${this.nombre} es` 
+    }
+}
+let animal1
+let nombreAnimal
+let edadAnimal
+document.getElementById('miFormulario3').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    nombreAnimal = document.getElementById('nombreAnimal').value;
+    edadAnimal = document.getElementById('nombreAnimal').value;
+    animal1=new Animal(nombreAnimal,nombreAnimal);
+    document.getElementById("imprimirSonidoAnimal").textContent=animal1.hacerSonido()
+});
+
+class Perro extends Animal{
+    constructor(nombre,edad,raza){
+        super(nombre,edad)
+        this.raza=raza;
+    }
+    moverCola(){
+        return `El perro ${this.nombre} con edad de ${this.edad} y raza ${this.raza} está moviendo la cola.` 
+    }
+}
+let perro1
+let nombrePerro
+let edadPerro
+let raza
+document.getElementById('miFormulario4').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    nombrePerro = document.getElementById('nombrePerro').value;
+    edadPerro = document.getElementById('edadPerro').value;
+    raza = document.getElementById('raza').value;
+    perro1=new Perro(nombrePerro,edadPerro,raza);
+    document.getElementById("imprimirPerro").textContent=perro1.moverCola()
+});
+
 
